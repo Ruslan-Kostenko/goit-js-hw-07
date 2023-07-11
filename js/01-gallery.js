aca298aa1d9275c836ee1935a92fd697f6b58c3c
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-07-12 00:14:29 +0300

    hw-07-js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.addEventListener('click', onItemClick);
+
+function onItemClick(event){
+    if(!event.target.classList.contains('gallery__image')){
+        return;
+    };
+    event.preventDefault();
+    console.log(event.target.dataset.source);
+    
+    const instance = basicLightbox.create(`
+        <img src ="${event.target.dataset.source}" width="800" height="600">
+    `);
+    instance.show();
+    
+};
+
+
+
+const listItemsGallery = createlistItemsGallery(galleryItems);
+
+function createlistItemsGallery(items) {
+    return items
+        .map(item => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
+        </a> </li>`)
+        .join('');
+}
+
+galleryListEl.innerHTML = listItemsGallery;
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryListEl = document.querySelector('.gallery');
+document.addEventListener('DOMContentLoaded', onItemClick);
+
+function onItemClick(event) {
+    event.preventDefault();
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionsDelay: 250,
+        enableKeyboard: true,
+    });
+};
+
+
+
+const listItemsGallery = createlistItemsGallery(galleryItems);
+
+function createlistItemsGallery(items) {
+    return items
+        .map(item => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
+        </a> </li>`)
+        .join('');
+}
+
+galleryListEl.insertAdjacentHTML("beforeend", listItemsGallery);
